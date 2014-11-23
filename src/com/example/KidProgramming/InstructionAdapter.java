112fdd04f35c0be4a69cf510d0644b18ca4a494e
ngocthanhcr <ngocthanhcr2000@gmail.com>
2014-11-23 07:17:12 +0700

    delete multi item

--- src/com/example/KidProgramming/InstructionAdapter.java
+++ src/com/example/KidProgramming/InstructionAdapter.java
@@ -1,52 +1,80 @@
 package com.example.KidProgramming;
 
 import android.app.Activity;
+import android.content.Context;
 import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by thanh on 11/14/14.
  */
-public class InstructionAdapter extends BaseAdapter {
+public class InstructionAdapter extends ArrayAdapter<Instruction> {
     Activity mActivity;
-    ArrayList<Instruction> mArrayInstruction;
-    public InstructionAdapter(Activity mActivity, ArrayList<Instruction> mArrayInstruction) {
+    List<Instruction> lInstructions;
+    private SparseBooleanArray mSelectedItemsIds;
+
+    public InstructionAdapter(Activity mActivity, int resId, List<Instruction> mArrayInstruction) {
+        super(mActivity, resId, mArrayInstruction);
+        mSelectedItemsIds = new SparseBooleanArray();
         this.mActivity = mActivity;
-        this.mArrayInstruction=mArrayInstruction;
+        this.lInstructions=mArrayInstruction;
         Log.i("Thanh","InstructionAdapter");
-    }
-
-    @Override
-    public int getCount() {
-        return mArrayInstruction.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return mArrayInstruction.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = mActivity.getLayoutInflater();
         View rowView= inflater.inflate(R.layout.instruction_layout, null, true);
-        Instruction mInstruction=(Instruction)getItem(position);
+        Instruction iInstruction=getItem(position);
         TextView textInstruction= (TextView) rowView.findViewById(R.id.text_instruction);
         ImageView imgInstruction= (ImageView) rowView.findViewById(R.id.img_instruction);
-        textInstruction.setText(mInstruction.getNameInstruction());
-        imgInstruction.setImageResource(mInstruction.getImageId());
+        textInstruction.setText(iInstruction.getNameInstruction());
+        imgInstruction.setImageResource(iInstruction.getImageId());
         return rowView;
+    }
+
+    @Override
+    public void remove(Instruction object) {
+        lInstructions.remove(object);
+        notifyDataSetChanged();
+    }
+
+    public List<Instruction> getInstructions()
+    {
+        return lInstructions;
+    }
+
+    public void toggleSelection(int position) {
+        selectView(position, !mSelectedItemsIds.get(position));
+    }
+
+    public void removeSelection() {
+        mSelectedItemsIds = new SparseBooleanArray();
+        notifyDataSetChanged();
+    }
+
+    public void selectView(int position, boolean value) {
+        if (value)
+            mSelectedItemsIds.put(position, value);
+        else
+            mSelectedItemsIds.delete(position);
+        notifyDataSetChanged();
+    }
+
+    public int getSelectedCount() {
+        return mSelectedItemsIds.size();
+    }
+
+    public SparseBooleanArray getSelectedIds() {
+        return mSelectedItemsIds;
     }
 }
--- src/com/example/KidProgramming/MainActivity.java
+++ src/com/example/KidProgramming/MainActivity.java
@@ -3,11 +3,19 @@ package com.example.KidProgramming;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+
+import static android.widget.AbsListView.CHOICE_MODE_MULTIPLE_MODAL;
 
 public class MainActivity extends Activity {
     /**
@@ -23,9 +31,67 @@ public class MainActivity extends Activity {
         //Instruction mInstruction=new Instruction(R.drawable.routing_forward,"di thang");
         mArrayInstruction=new ArrayList<Instruction>();
         //mArrayInstruction.add(mInstruction);
-        final InstructionAdapter mInstructionAdapter=new InstructionAdapter(this,mArrayInstruction);
+        final InstructionAdapter mInstructionAdapter=new InstructionAdapter(this, R.layout.instruction_layout, mArrayInstruction);
         listInstruction=(ListView)findViewById(R.id.list_instruction);
         listInstruction.setAdapter(mInstructionAdapter);
+        listInstruction.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Instruction selecteditem = mInstructionAdapter.getItem(position);
+                mInstructionAdapter.remove(selecteditem);
+                mInstructionAdapter.notifyDataSetChanged();
+            }
+        });
+        listInstruction.setChoiceMode(CHOICE_MODE_MULTIPLE_MODAL);
+        listInstruction.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+            @Override
+            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+                // Capture total checked items
+                final int checkedCount = listInstruction.getCheckedItemCount();
+                // Set the CAB title according to total checked items
+                mode.setTitle(checkedCount + " Selected");
+                // Calls toggleSelection method from ListViewAdapter Class
+                mInstructionAdapter.toggleSelection(position);
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                mode.getMenuInflater().inflate(R.menu.activity_main,menu);
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.delete:
+                        // Calls getSelectedIds method from ListViewAdapter Class
+                        SparseBooleanArray selected = mInstructionAdapter.getSelectedIds();
+                        // Captures all selected ids with a loop
+                        for (int i = (selected.size() - 1); i >= 0; i--) {
+                            if (selected.valueAt(i)) {
+                                Instruction selecteditem = mInstructionAdapter.getItem(selected.keyAt(i));
+                                // Remove selected items following the ids
+                                mInstructionAdapter.remove(selecteditem);
+                            }
+                        }
+                        // Close CAB
+                        mode.finish();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+                mInstructionAdapter.removeSelection();
+            }
+        });
         Log.i("Thanh","onCreate");
         ImageButton ibtnTurnLeft=(ImageButton)findViewById(R.id.ibtn_turn_left);
         ImageButton ibtnForward=(ImageButton)findViewById(R.id.ibtn_forward);
